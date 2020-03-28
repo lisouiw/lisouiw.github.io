0b85dec5fdf1a61dff5c6603722717c455e1b3a3
Lisouiw <ltran@student42.fr>
2020-03-28 02:29:16 +0100

    add Studio ghibli demo

---
+++ precache-manifest.ae224ef0697b823dd345d31ce90dd972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c441b6b749af49cdc2c3e9a86c3bb862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d38375a70ffd20e6bdc",
+    "url": "/static/css/main.96df0fae.chunk.css"
+  },
+  {
+    "revision": "7631b4cef5ad3f90cb0f",
+    "url": "/static/js/2.d2786664.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d2786664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d38375a70ffd20e6bdc",
+    "url": "/static/js/main.66266ac0.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
