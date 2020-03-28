d6a3c6b20815535bc1dd625ed3e92511df1b33a2
Lisouiw <ltran@student42.fr>
2020-03-28 02:02:52 +0100

    add Studio ghibli demo

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20a32b2d8ccecb9e9b726afc475d4864.js"
+  "/precache-manifest.e7d78fe45e045b4ce1f8c2b49df3c1a6.js"
 );
 
 self.addEventListener('message', (event) => {
