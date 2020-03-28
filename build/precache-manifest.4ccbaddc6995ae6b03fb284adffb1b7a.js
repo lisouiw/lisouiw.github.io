1024fb5a3328e19b606ad9023da663fd5070a4f6
Lisouiw <ltran@student42.fr>
2020-03-28 02:15:37 +0100

    add Studio ghibli demo

---
+++ build/precache-manifest.4ccbaddc6995ae6b03fb284adffb1b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3c4974d9289259b456d3bf0871c7b4",
+    "url": "https://lisouiw.github.io/index.html"
+  },
+  {
+    "revision": "c231c8d94ad99642571d",
+    "url": "https://lisouiw.github.io/static/css/main.96df0fae.chunk.css"
+  },
+  {
+    "revision": "30d9152b6a1f229cda76",
+    "url": "https://lisouiw.github.io/static/js/2.a116cbf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://lisouiw.github.io/static/js/2.a116cbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c231c8d94ad99642571d",
+    "url": "https://lisouiw.github.io/static/js/main.48b8ceb6.chunk.js"
+  },
+  {
+    "revision": "f830704ad25b64005a93",
+    "url": "https://lisouiw.github.io/static/js/runtime-main.bee41f32.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94545977d16db2867816d0bbd9b9fa93.js"
+  "https://lisouiw.github.io/precache-manifest.4ccbaddc6995ae6b03fb284adffb1b7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://lisouiw.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
